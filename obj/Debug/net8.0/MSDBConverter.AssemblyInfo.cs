81d480f1c28a9ace1c7256195e09476a5292e657
niccoloc0 <niccoloc20@gmail.com>
2024-07-22 23:38:47 +0200

    Added a new icon.

--- obj/Debug/net8.0/MSDBConverter.AssemblyInfo.cs
+++ obj/Debug/net8.0/MSDBConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSDBConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edfe27f621d45c15f270829261b817553acd823")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fec5a202dbe5b31b3aab8644e5b857651bab114")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSDBConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSDBConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
